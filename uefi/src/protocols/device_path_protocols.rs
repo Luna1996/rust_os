3e3ea3c8e0702c9353760ecb0239ba02b44253aa
l+l l +K <2320909844@qq.com>
2020-02-12 17:16:55 -0600

    device path protocol

--- uefi/src/protocols/device_path_protocols.rs
+++ uefi/src/protocols/device_path_protocols.rs
@@ -70,3 +70,41 @@ pub type EFI_DEVICE_PATH_UTILS_CREATE_NODE<'a> = extern "C" fn(
 pub type EFI_DEVICE_PATH_UTILS_IS_MULTI_INSTANCE = extern "C" fn(
   DevicePath: &EFI_DEVICE_PATH_PROTOCOL, // IN
 ) -> bool;
+
+// EFI_DEVICE_PATH_TO_TEXT_PROTOCOL
+pub const EFI_DEVICE_PATH_TO_TEXT_PROTOCOL_GUID: EFI_GUID = 0x8b843e208132485290cc551a4e4a7f1c;
+
+#[repr(C)]
+pub struct _EFI_DEVICE_PATH_TO_TEXT_PROTOCOL {
+  pub ConvertDeviceNodeToText: EFI_DEVICE_PATH_TO_TEXT_NODE,
+  pub ConvertDevicePathToText: EFI_DEVICE_PATH_TO_TEXT_PATH,
+}
+
+pub type EFI_DEVICE_PATH_TO_TEXT_NODE = extern "C" fn(
+  DeviceNode: &EFI_DEVICE_PATH_PROTOCOL, // IN
+  DisplayOnly: bool,                     // IN
+  AllowShortcuts: bool,                  // IN
+) -> *const u16;
+
+pub type EFI_DEVICE_PATH_TO_TEXT_PATH = extern "C" fn(
+  DevicePath: &EFI_DEVICE_PATH_PROTOCOL, // IN
+  DisplayOnly: bool,                     // IN
+  AllowShortcuts: bool,                  // IN
+) -> *const u16;
+
+// EFI_DEVICE_PATH_FROM_TEXT_PROTOCOL
+pub const EFI_DEVICE_PATH_FROM_TEXT_PROTOCOL_GUID: EFI_GUID = 0x5c99a21c70f4ad28a5f35df3343f51e;
+
+#[repr(C)]
+pub struct _EFI_DEVICE_PATH_FROM_TEXT_PROTOCOL<'a> {
+  pub ConvertTextToDevicNode: EFI_DEVICE_PATH_FROM_TEXT_NODE<'a>,
+  pub ConvertTextToDevicPath: EFI_DEVICE_PATH_FROM_TEXT_PATH<'a>,
+}
+
+pub type EFI_DEVICE_PATH_FROM_TEXT_NODE<'a> = extern "C" fn(
+  TextDeviceNode: *const u16, // IN
+) -> &'a EFI_DEVICE_PATH_PROTOCOL;
+
+pub type EFI_DEVICE_PATH_FROM_TEXT_PATH<'a> = extern "C" fn(
+  TextDevicePath: *const u16, // IN
+) -> &'a EFI_DEVICE_PATH_PROTOCOL;
